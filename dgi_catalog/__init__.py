0b082095f15867ee73cf573e1189be88f83e75f2
Rodrigo Mariano <rodmariano13@gmail.com>
2019-11-15 16:04:10 -0300

    Added logger

--- dgi_catalog/__init__.py
+++ dgi_catalog/__init__.py
@@ -1,13 +1,14 @@
 """DGI Catalog"""
 
-import os
+from os import environ as os_environ
+
 from flask import Flask
+from flask.logging import create_logger, logging
+
 from flask_cors import CORS
+
 from dgi_catalog.blueprint import blueprint
 from dgi_catalog.config import get_settings
-
-
-ENV = os.environ.get('ENV', 'development')
 
 
 def create_app(config):
@@ -29,9 +30,22 @@ def create_app(config):
     return internal_app
 
 
+ENV = os_environ.get('ENV', 'development')
+
+# create Flask app
 app = create_app(get_settings(ENV))
 
-# debug mode if ENV == 'development'
+# set debug mode to True if ENV == 'development'
 app.debug = True if ENV == 'development' else False
+# app.debug = False
 
 CORS(app, resorces={r'/d/*': {"origins": '*'}})
+
+# create Flask logger
+logger = create_logger(app)
+logger.setLevel(logging.INFO)
+
+logger.info('ENV: %s', ENV)
+# logger.warning('>>> 1 warning: %s', ENV)
+# logger.error('>>> 1 error: %s', ENV)
+# logger.critical('>>> 1 critical: %s', ENV)
